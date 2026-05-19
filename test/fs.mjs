d5f30ae7e2dc809ae3a6db939f357a934e80ffef
Wu-Yijun <wuyiun21@mails.ucas.ac.cn>
2026-05-19 18:46:37 +0800

    try linux

--- test/fs.mjs
+++ test/fs.mjs
@@ -15,6 +15,7 @@ test("test_filesystem", async () => {
     console.log("fs.locate", await fs.locate(TMP, OpenFlags.Refresh));
     console.log("fs.deeplocate", await fs.deepLocate(TMP, OpenFlags.Refresh));
     // console.log("fs.query", await fs.query(QueryCode.SPACE, '/tmp')); // TODO
+    console.log("fs.dirList", await fs.dirList(TMP));
     console.log("fs.truncate", await fs.truncate(TMP + "/spam", 200));
     console.log("fs.move", await fs.mv( TMP + "/spam", TMP + "/ham"));
     console.log("fs.chmod", await fs.chmod( TMP + "/ham", AccessMode.UR | AccessMode.UW ));
@@ -25,7 +26,6 @@ test("test_filesystem", async () => {
     console.log("fs.stat", await fs.stat(TMP));
     console.log("fs.statVFS", await fs.statVFS(TMP));
     console.log("fs.protocol", await fs.protocol());
-    console.log("fs.dirList", await fs.dirList(TMP));
     console.log("fs.sendInfo", await fs.sendInfo("[NOTICE] important info"));
     console.log("fs.prepare", await fs.prepare([TMP + "/bigfile.bin"]));
 });
