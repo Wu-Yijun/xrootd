299ceb5bdac2951339bf86d04bd11c25c7fae8eb
Wu-Yijun <wuyiun21@mails.ucas.ac.cn>
2026-05-19 18:01:11 +0800

    fix test script for alive checker

--- test/env.mjs
+++ test/env.mjs
@@ -10,7 +10,8 @@
 // bigfile     = SERVER_URL + '/tmp/bigfile'
 // bigcopy     = SERVER_URL + '/tmp/bigcopy'
 
-import { execSync } from "node:child_process";
+// import { execSync } from "node:child_process";
+import { createConnection } from "node:net";
 
 const PORT = 32874;
 export const SERVER_URL = `root://localhost:${PORT}/`;
@@ -21,13 +22,24 @@ export const smallbuffer = 'gre\0en\neggs\nand\nham\n';
 export const bigfile = SERVER_URL + PWD + '/tmp/bigfile';
 export const bigcopy = SERVER_URL + PWD + '/tmp/bigcopy';
 
-export function testAlive(port = PORT) {
-    try {
-        execSync(`nc -zv localhost 32874`)
-        return true;
-    } catch (e) {
-        console.log(`[ERROR] Could Not connect to localhost:${port}!`);
-        console.error(e.message);
-    }
-    return false;
+export async function testAlive(port = PORT) {
+  return new Promise((a, r) => {
+    const c = setTimeout(() => {
+      console.error("Connection Timeout!");
+      a(false);
+    }, 15000);
+    const socket = createConnection(port, "localhost", () => {
+      console.log(`✅ Success: XRootD server is actively hosting on port ${port}`);
+      socket.end(); // Close the connection cleanly
+      clearTimeout(c);
+      a(true);
+    });
+    let success = true;
+    socket.on('error', (err) => {
+      console.error(`❌ Failure: Could not connect to port ${port}. Is the server running?`);
+      console.error(err);
+      clearTimeout(c);
+      a(false);
+    });
+  });
 }
